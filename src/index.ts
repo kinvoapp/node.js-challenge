196c395bc8f4ab827afa034a759b04d41b96010d
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-07 19:06:26 -0300

    style

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import Routes from './routes/route'
+import Routes from './routes/finance'
 
 
 
 const app = express()
 
 app.use(Routes)
+
 app.use((req, res) => {
     res.status(404)
 })
