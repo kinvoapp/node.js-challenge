eb232cf2d0e7007de39fb5b1870e925e60840f40
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-08 04:52:51 -0300

    chore: add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express'
 import Routes from './routes/finance'
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://pedrobuenoxs:12345@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority')
+.then( () => {
+    console.log('Db connected')
+}).catch((err) => {
+    console.log('Erro while connecting database::', err)
+})
 
 const app = express()
 app.use(express.json())
