0684307625ca8cd9ce397c1572b64aebef6c5547
gervasioartur <gervasioartur@outlook.com.br>
2022-07-08 09:15:21 -0300

    test: ensure BcryptHanlder retrhow if hash throws

--- tests/infra/crypto/bcrypt-handler.spec.ts
+++ tests/infra/crypto/bcrypt-handler.spec.ts
@@ -3,22 +3,27 @@ import bcrypt from 'bcrypt'
 jest.mock('bcrypt')
 
 type Input = { value: string }
+type Output = string
 
 export class BcryptHandler {
   constructor (private readonly salt: number) { }
-  async encrypt (params: Input): Promise<void> {
-    await bcrypt.hash(params.value, this.salt)
+  async encrypt (params: Input): Promise<Output> {
+    const hashedPassword = await bcrypt.hash(params.value, this.salt)
+    return hashedPassword
   }
 }
 
 describe('BcryptHandler', () => {
   let salt: number
+  let hashedPassword: string
   let sut: BcryptHandler
   let fakeBcrypt: jest.Mocked<typeof bcrypt>
 
   beforeAll(() => {
     salt = 12
+    hashedPassword = 'any_hashed_passoword'
     fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>
+    fakeBcrypt.hash.mockImplementation(() => hashedPassword)
     sut = new BcryptHandler(salt)
   })
 
@@ -26,5 +31,16 @@ describe('BcryptHandler', () => {
     await sut.encrypt({ value: 'any_value' })
     expect(fakeBcrypt.hash).toHaveBeenCalledWith('any_value', salt)
     expect(fakeBcrypt.hash).toBeCalledTimes(1)
+  })
+
+  it('should return a passwordHashed on success', async () => {
+    const passwordHashed = await sut.encrypt({ value: 'any_value' })
+    expect(passwordHashed).toBe(hashedPassword)
+  })
+
+  it('should rethrow if hash throws', async () => {
+    fakeBcrypt.hash.mockImplementationOnce(() => { throw new Error('token_error') })
+    const promise = sut.encrypt({ value: 'any_value' })
+    await expect(promise).rejects.toThrow(new Error('token_error'))
   })
 })
