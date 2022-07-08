decce0fa22b94ebb633a5438cf5c69493fbb7fee
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-08 04:52:06 -0300

    chore: update

--- server.ts
+++
@@ -1,9 +0,0 @@
-import app from './src/index'
-
-const PORT = process.env.PORT || 3000
-
-const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
-})
