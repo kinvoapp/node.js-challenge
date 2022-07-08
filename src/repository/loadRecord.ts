14b8dadf610204aed82f202f20ebd1fe8e29e965
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-08 07:28:32 -0300

    chore: add filter for period

--- src/repository/loadRecord.ts
+++ src/repository/loadRecord.ts
@@ -1,9 +1,16 @@
 
+import { Request } from 'express'
 import Record from '../models/recordModel'
 
 
-const loadRecord = () => {
-   return Record.find()
+const loadRecord = (Request:Request) => {
+const {startDate,endDate} = Request.params
+
+   return Record.find({
+      "createdAt": {
+        '$gte': `${startDate}T00:00:00.000Z`,
+        '$lt': `${endDate}T23:59:59.999Z`
+      }})
 }
 
 export default loadRecord
