fd27becf74bce1431c6e1225436209e941420e66
Wendres Lucas <wendreslucas@gmail.com>
2022-07-07 23:25:11 -0300

    model transaction

---
+++ src/models/transaction.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  created_datetime: {
+    type: Date,
+  },
+});
+
+export default mongoose.model("Transaction", transactionSchema);
