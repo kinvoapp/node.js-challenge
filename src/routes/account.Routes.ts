db642c7bcac973f7c3f71ee1c95d8982872f7011
Wendres Lucas <wendreslucas@gmail.com>
2022-07-08 17:50:45 -0300

    inicio do routes account

---
+++ src/routes/account.Routes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import AccountController from "../controllers/accountController";
+const router = express.Router();
+
+router.get("").get("").post("").put("").delete("");
+
+export default router;
