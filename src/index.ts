5e70d3de38a619053e4f00c4b3ae6ee004894c79
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-08 00:33:03 -0300

    style

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from 'express'
 import Routes from './routes/finance'
-
-
+import * as mongoose from 'mongoose';
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', Routes)
 
-app.use(Routes)
-
-app.use((req, res) => {
-    res.status(404)
-})
 
 
 
