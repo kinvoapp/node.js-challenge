2ed0d4455baef9ef89569700c44d70f6cfb6b4a3
Wendres Lucas <wendreslucas@gmail.com>
2022-07-07 23:33:06 -0300

    account model

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+  },
+});
+
+export default mongoose.model("Account", accountSchema);
