03ea8d8c953071a2dfa2b011e81fe1cf4ee3c764
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-09 18:02:28 -0300

    Created test for /movement endpoint

--- src/tests/server.spec.ts
+++ src/tests/server.spec.ts
@@ -5,9 +5,15 @@ const request = supertest('http://localhost:8080')
 describe('must checkt the server', () => {
   // For this test, the server must be started or in development mode.
   test('must check port 8080', async () => {
-    const res = await request.get('/Hello%20Word')
+    const res = await request.get('/hello/Hello%20Word')
 
     expect(res.status).toBe(200)
     expect(res.body).toStrictEqual({ message: 'Hello Word' })
+  })
+
+  test('must check the /movement endpoint', async () => {
+    const res = await request.get('/movement')
+
+    expect(res.status).toBe(200)
   })
 })
