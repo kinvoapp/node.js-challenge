59808cca8bdc33488b7c44b14749a15905f09810
fiusks <rafabarros1@hotmail.com>
2022-07-10 18:53:19 -0300

    FIX simplified transactions and user models

--- src/models/transactions.ts
+++ src/models/transactions.ts
@@ -1,24 +1,9 @@
-import { IResponse } from "./api"
-import { RequestHandler } from "express";
-import { ParamsDictionary } from 'express-serve-static-core'
-
-
 export interface ITransaction {
     id?: string,
-    amount: number | string,
+    amount: number,
     type: "credit" | "debit"
     date: Date
     user_id: string
     created_at?: Date
     updated_at?: Date
 }
-
-export type TransactionAPI = RequestHandler<ParamsDictionary,
-    IResponse<TransactionResponseMessage | ITransaction[]>,
-    ITransaction>
-
-
-export type TransactionResponseMessage =
-    "Transaction created successfully" |
-    "Transaction edited successfully" |
-    "Transaction deleted successfully"
