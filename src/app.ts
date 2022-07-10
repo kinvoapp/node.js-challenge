81f98839e67f97e9c063120a6438fd7c64b7132e
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-10 12:06:41 -0300

    style

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
-import express from 'express'
-import Routes from './routes/finance-router'
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv';
+import express from "express";
+import Routes from "./routes/finance-router";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
-mongoose.connect('mongodb+srv://pedrobuenoxs:12345@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority')
-.then( () => {
-    console.log('Db connected')
-}).catch((err) => {
-    console.log('Erro while connecting database::', err)
-})
+mongoose
+  .connect(
+    "mongodb+srv://pedrobuenoxs:12345@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log("Error while connecting database::", err);
+  });
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api', Routes)
-dotenv.config()
-
-
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", Routes);
+dotenv.config();
 
 export default app;
--- src/controller/finance-controller.ts
+++ src/controller/finance-controller.ts
@@ -3,14 +3,26 @@ import {createRecord, updateRecord, deleteRecord, loadRecord, balanceAmount} fro
 import { calculate } from '../utils/calcBalance';
 
     const addTransaction = (req: Request, res: Response, next: NextFunction) => {
-        createRecord(req).then(result =>{
-            return res.status(201).json({
-                record: result
+        const { desc, type, value, note } = req.body 
+
+        try {
+            if(!req.body) return res.status(400).json({error: 'your body params are'})
+            if(!desc) return res.status(400).json({error: 'the description is required'})
+            if(!type) return res.status(400).json({error: 'the type is required'})
+            if(!value) return res.status(400).json({error: 'the value is required'})
+    
+            createRecord(req).then(result =>{
+                return res.status(201).json({
+                    record: result
+                })
             })
-        })
-        .catch((err) => {
-            return err
-        })
+            .catch((err) => {
+                return err
+            })            
+        } catch (error) {
+            res.status(500).json({error: 'something went wrong.'})
+            next()
+        }
     }
 
     const updateTransaction = (req: Request, res: Response, next: NextFunction) => {
