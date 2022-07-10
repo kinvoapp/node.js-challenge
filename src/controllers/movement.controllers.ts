53ea7b5d8bc87e598d77efb23bc7a172219f8b06
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-10 15:12:30 -0300

    Created controller for update movement

--- src/controllers/movement.controllers.ts
+++ src/controllers/movement.controllers.ts
@@ -17,7 +17,7 @@ import { IMovement } from 'types'
  * @param req is an Request from express.
  * @param res is an Response from express.
  * @param next is an NextFuncion from express.
- * @returns {Promise<Response<IMovement, Record<string, IMovement>>>} can return status 422 errors or if the request is successful returns an object of type IMovement.
+ * @returns {Promise<Response<IMovement, Record<string, IMovement>>>} a promise from an IMovement.
  */
 async function createMovement (req: Request, res: Response, next: NextFunction): Promise<Response<IMovement, Record<string, IMovement>>> {
   const {
@@ -92,6 +92,16 @@ async function getMovements (_: Request, res: Response, next: NextFunction): Pro
   }
 }
 
+/**
+ * **getMovement**
+ * is an asynchronous function that searches for a movement by its id.
+ *
+ * @param req is an Request from express.
+ * @param res is an Response from express.
+ * @param next is an NextFuncion from express.
+ * @returns {Promise<Response<IMovement, Record<string, IMovement>>>} a promise from an IMovement.
+ */
+
 async function getMovement (req: Request, res: Response, next: NextFunction): Promise<Response<IMovement, Record<string, IMovement>>> {
   const { id } = req.params
   try {
@@ -115,8 +125,65 @@ async function getMovement (req: Request, res: Response, next: NextFunction): Pr
   }
 }
 
+/**
+ * **updateMovement**
+ * is an asynchronous function that does motion update by its `id`
+ * and a new `movement` object of type IMovement.
+ *
+ * @param req is an Request from express.
+ * @param res is an Response from express.
+ * @param next is an NextFuncion from express.
+ * @returns {Promise<Response<IMovement, Record<string, IMovement>>>} a promise from an IMovement.
+ */
+
+async function updateMovement (req: Request, res: Response, next: NextFunction): Promise<Response<IMovement, Record<string, IMovement>>> {
+  const { id } = req.params
+
+  const {
+    type,
+    value,
+    category,
+    date,
+    note
+  } = req.body
+  try {
+    if (!type) return res.status(422).json({ error: 'the type field is required' })
+    if (type !== 'expense' && type !== 'income') return res.status(422).json({ error: 'unrecognized type' })
+    if (!value) return res.status(422).json({ error: 'the value field is required' })
+    if (!category) return res.status(422).json({ error: 'the category field is required' })
+    if (!date) return res.status(422).json({ error: 'the date field is required' })
+
+    const movement: IMovement = {
+      type,
+      value,
+      category,
+      date,
+      note
+    }
+
+    const updateMovement = await MovementServices.updateMovement(id, movement)
+
+    if (updateMovement.matchedCount === 0) {
+      return res.status(422).json({ error: 'movement not found.' })
+    }
+
+    return res.status(201).json({
+      id,
+      type,
+      value,
+      category,
+      date,
+      note
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+    next(error)
+  }
+}
+
 export default {
   createMovement,
   getMovements,
-  getMovement
+  getMovement,
+  updateMovement
 }
