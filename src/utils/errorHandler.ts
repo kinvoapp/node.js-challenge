80b757214e7774b0c529a3ab7bc7220396e67dbc
fiusks <rafabarros1@hotmail.com>
2022-07-10 19:01:20 -0300

    DEL unused util function

--- src/utils/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-export function errorHandler(error: any): { status?: number, errorData?: string[] } {
-    const { error: { status } } = error
-    if (status) {
-        return { status, errorData: error }
-    }
-    return {}
-}
