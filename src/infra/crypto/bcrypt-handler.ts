98452215babff4d90f39a6bdb3d9445ae806ba80
gervasioartur <gervasioartur@outlook.com.br>
2022-07-10 11:03:17 -0300

    test: ensure bcrypt handler return a boolean value on compare sucess

--- src/infra/crypto/bcrypt-handler.ts
+++ src/infra/crypto/bcrypt-handler.ts
@@ -1,13 +1,24 @@
 import { Encrypter } from '@/domain/contracts/crypto'
 import bcrypt from 'bcrypt'
+namespace encrypt {
+  export type Input = { value: string }
+  export type Output = { key: string }
+}
 
-type Input = { value: string }
-type Output = { key: string }
+namespace compare {
+  export type Input = { value: string, valueToCompare: string }
+  export type Output = boolean
+}
 
 export class BcryptHandler implements Encrypter {
   constructor (private readonly salt: number) { }
-  async encrypt (params: Input): Promise<Output> {
+
+  async encrypt (params: encrypt.Input): Promise<encrypt.Output> {
     const hashedPassword = await bcrypt.hash(params.value, this.salt)
     return { key: hashedPassword }
+  }
+
+  async compare (params: compare.Input): Promise<compare.Output> {
+    return await bcrypt.compare(params.value, params.valueToCompare)
   }
 }
