9e1e1ee414e466c244770223b0ca617dc1842a9b
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-07-10 05:38:22 -0300

    refactor: Deleted unnecessary items.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, ItemModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
