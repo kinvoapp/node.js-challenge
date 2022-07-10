0c6c2c62c14951a672695f0dba197497f8d2bb3a
fiusks <rafabarros1@hotmail.com>
2022-07-10 18:44:32 -0300

    NEW Swagger implementation

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@
 import express from 'express'
 import cors from 'cors'
 import router from './routes/router'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 const app = express()
-
-// dotenv.config()
 
 app.use(cors())
 
 app.use(express.json())
 
-app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+app.use("/api", router);
 
 export default app
