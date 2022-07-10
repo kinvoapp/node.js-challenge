d67f0b1e19a09214c0e4cf43c16b062251bc79ee
Gustavo Viana <gustavofvv@gmail.com>
2022-07-10 23:36:24 +0000

    feat: swagger complete workflow

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import { errorHandler } from "./middlewares/errorHandler";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
