77abd96a7e6201ece48cfbbc2e28f3e37eea6825
gervasioartur <gervasioartur@outlook.com.br>
2022-07-10 13:15:34 -0300

    test: ensure service return a server error on aff fails

--- tests/domain/use-cases/add-finantial-income.spec.ts
+++ tests/domain/use-cases/add-finantial-income.spec.ts
@@ -1,5 +1,12 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
+export class ServerError extends Error {
+  constructor () {
+    super('An error occured with trying save the data.')
+    this.name = 'ServerError'
+  }
+}
+
 export namespace AddFinantialIncomeRepository {
   export type Input = { type: string, value: number, description: string, userId: string }
   export type Output = { id: string, type: string, value: number, description: string, userId: string }
@@ -10,12 +17,14 @@ export interface AddFinantialIncomeRepository {
 }
 
 type Input = { type: string, value: number, description: string, userId: string }
-export type Output = { id: string, type: string, value: number, description: string }
+type Output = undefined | { id: string, type: string, value: number, description: string, userId: string }
 
 type AddFinantialIncome = (params: Input) => Promise<Output>
 export type Setup = (bankAccoutRepo: AddFinantialIncomeRepository) => AddFinantialIncome
 export const setupAddFinantialIncome: Setup = (bankAccoutRepo) => async params => {
-  return await bankAccoutRepo.add({ type: params.type, value: params.value, description: params.description, userId: params.userId })
+  const result = await bankAccoutRepo.add({ type: params.type, value: params.value, description: params.description, userId: params.userId })
+  if (result !== undefined) return result
+  throw new ServerError()
 }
 
 describe('AddFinantialIncome', () => {
@@ -52,5 +61,11 @@ describe('AddFinantialIncome', () => {
       description: 'any_desc',
       userId: 'any_user_id'
     })
+  })
+
+  it('should  rethrow if add throws', async () => {
+    bankAccoutRepo.add.mockRejectedValueOnce(new Error('An error occured with trying save the data.'))
+    const promise = sut({ type: 'any_type', value: 1000, description: 'any_desc', userId: 'any_user_id' })
+    await expect(promise).rejects.toThrow(new Error('An error occured with trying save the data.'))
   })
 })
