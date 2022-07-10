0fb9e343d9cc8861c30a0ca9e756c0da7adcdfc9
Matheus Barboza <matheus.barboza102@gmail.com>
2022-07-10 01:18:06 -0300

    config eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
