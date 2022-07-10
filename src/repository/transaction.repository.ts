da8e8d24af12d89b24b615bf60dcdd2573ccf586
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-10 18:00:57 -0300

    add updateRecord

--- src/repository/transaction.repository.ts
+++ src/repository/transaction.repository.ts
@@ -3,12 +3,16 @@ import Record from "../models/recordModel";
 import mongoose from "mongoose";
 
 export class TransactionRepository {
-  async saveRecord(data: TransactionDTO) {
+  saveRecord(data: TransactionDTO) {
     const record = new Record({
       _id: new mongoose.Types.ObjectId(),
       ...data,
     });
     const save = record.save();
     return save;
+  }
+
+  updateRecord(id: string, data: TransactionDTO) {
+    return Record.findByIdAndUpdate(id, data);
   }
 }
