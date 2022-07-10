b2510549ce23a0203beb023a0546a0462bf15430
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-07-10 14:50:54 -0300

    test: In the `npm run test:cov` command, Created 100% test coverage for the `user` controller.

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
-import 'reflect-metadata';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { createUserDto } from './dto/user.dto';
+import 'reflect-metadata';
+import { createResponse } from 'node-mocks-http';
 
 // https://www.youtube.com/watch?v=p1hzHVgG6bI
 const userEntityList: User[] = [
@@ -13,6 +15,13 @@ const userEntityList: User[] = [
     password: '123456',
   }),
 ];
+
+const newUserEntity: User = new User({
+  id: 2,
+  name: 'Jao Santos',
+  email: 'jao@email.com',
+  password: '$2b$08$4BJUIevoPBbNWITBtRj2IePWSowvOtt2WNxGrdI2kZk9tiI2HwfP2',
+});
 
 describe('UserController', (): void => {
   let userController: UserController;
@@ -26,8 +35,8 @@ describe('UserController', (): void => {
           provide: UserService,
           useValue: {
             findAll: jest.fn().mockResolvedValue(userEntityList),
-            createUser: jest.fn(),
-            deleteUser: jest.fn(),
+            createUser: jest.fn().mockResolvedValue(newUserEntity),
+            deleteUser: jest.fn().mockResolvedValue(undefined),
           },
         },
       ],
@@ -54,6 +63,56 @@ describe('UserController', (): void => {
       jest.spyOn(userService, 'findAll').mockRejectedValue(new Error('Error'));
 
       await expect(userController.findAll()).rejects.toThrowError('Error');
+    });
+  });
+
+  describe('createUser', (): void => {
+    it('should return a new user, successfully', async (): Promise<void> => {
+      const body: createUserDto = new User({
+        name: 'Jao Santos',
+        email: 'jao@email.com',
+        password: '123456',
+      });
+
+      const res = createResponse();
+      await userController.createUser(res, body);
+
+      expect(res._getJSONData()).toEqual(newUserEntity);
+      expect(userService.createUser).toHaveBeenCalledTimes(1);
+      expect(userService.createUser).toHaveBeenCalledWith(body);
+    });
+
+    it('should throm an expected error', async (): Promise<void> => {
+      jest.spyOn(userService, 'createUser').mockRejectedValueOnce(new Error());
+      const body: createUserDto = new User({
+        name: 'Jao Santos',
+        email: 'jao@email.com',
+        password: '123456',
+      });
+      const res = createResponse();
+
+      expect(userController.createUser(res, body)).rejects.toThrowError();
+    });
+  });
+
+  describe('deleteUser', (): void => {
+    it('should return a deleted user, successfully', async (): Promise<void> => {
+      const id = '1';
+
+      const res = createResponse();
+      await userController.deleteUser(res, { id });
+
+      expect(res._getJSONData()).toEqual({});
+      expect(userService.deleteUser).toHaveBeenCalledTimes(1);
+      expect(userService.deleteUser).toHaveBeenCalledWith(id);
+    });
+
+    it('should throm an expected error', (): void => {
+      jest.spyOn(userService, 'deleteUser').mockRejectedValueOnce(new Error());
+      const id = '1';
+      const res = createResponse();
+
+      expect(userController.deleteUser(res, { id })).rejects.toThrowError();
     });
   });
 });
