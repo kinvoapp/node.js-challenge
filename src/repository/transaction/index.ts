622cd0f3529f9305be04ba35d62d8d112fb92f28
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-09 15:08:51 -0300

    feat: add a factory to transaction repository for better mocking and testing

--- src/repository/transaction/index.ts
+++ src/repository/transaction/index.ts
@@ -2,10 +2,13 @@ import createTransaction from "./create-transaction";
 import findTransaction from "./find-transaction";
 import updateTransaction from "./update-transaction";
 import deleteTransaction from "./delete-transaction";
+import transactionRepositoryFactory from "@/factory/repository/transaction";
+import findOneTransaction from "./find-one-transaction";
 
-export const transactionRepository = {
+export const transactionRepository = transactionRepositoryFactory({
   create: createTransaction,
   find: findTransaction,
+  findOne: findOneTransaction,
   update: updateTransaction,
   delete: deleteTransaction,
-};
+});
