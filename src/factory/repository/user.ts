91891029d64534a31e6645beb1152747b8e10d08
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-11 14:27:02 -0300

    feat: create user repository factory

---
+++ src/factory/repository/user.ts
@@ -0,0 +1,14 @@
+import { UserRepository } from "@/internal/interface/repository/user";
+
+export default function userRepositoryFactory(
+  repositoryMethods: UserRepository,
+): UserRepository {
+  return {
+    create: repositoryMethods.create,
+    find: repositoryMethods.find,
+    findCount: repositoryMethods.findCount,
+    findOne: repositoryMethods.findOne,
+    update: repositoryMethods.update,
+    delete: repositoryMethods.delete,
+  };
+}
