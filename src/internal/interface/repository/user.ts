da2928ed0a51a55845b1e08ae722dd6ae00ea6b2
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-12 02:03:39 -0300

    refactor: update input data type for findOne property on user repository interface

--- src/internal/interface/repository/user.ts
+++ src/internal/interface/repository/user.ts
@@ -1,11 +1,16 @@
-import { CreateUserDto, FindUserDto, UpdateUserDto } from "@/internal/dto/user";
+import {
+  CreateUserDto,
+  FindOneUser,
+  FindUserDto,
+  UpdateUserDto,
+} from "@/internal/dto/user";
 import { user } from "@prisma/client";
 
 export interface UserRepository {
   create: (data: CreateUserDto) => Promise<user>;
   find: (data?: FindUserDto) => Promise<user[]>;
   findCount: () => Promise<number>;
-  findOne: (id: string) => Promise<user | null>;
+  findOne: (data: FindOneUser) => Promise<user | null>;
   update: (id: string, data: UpdateUserDto) => Promise<user>;
   delete: (id: string) => Promise<user>;
 }
